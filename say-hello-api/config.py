822b5a7b16c99ca941c4226b84d3268cb746a355
Melanie <mel.lontsi@gmail.com>
2022-12-13 23:06:35 -0800

    removed unnecessary env variables

--- say-hello-api/config.py
+++ say-hello-api/config.py
@@ -7,11 +7,6 @@ class BaseConfig:
     TESTING = False
     SECRET_KEY = os.environ['SAYHELLO_APP_SECRET_KEY']
     CACHE_TYPE = 'SimpleCache'
-    DB_HOST = os.environ['SAYHELLO_DB_HOST']
-    DB_PORT = os.environ['SAYHELLO_DB_PORT']
-    DB_NAME = os.environ['SAYHELLO_DB_NAME']
-    DB_USER = os.environ['SAYHELLO_DB_USER']
-    DB_PASSWORD = os.environ['SAYHELLO_DB_PASSWORD']
     DB_URI = os.environ['SAYHELLO_DB_URL']
 
 
